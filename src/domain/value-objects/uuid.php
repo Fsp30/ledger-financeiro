ad590dbdbd31d52849cac79e4511515d7b4c5584
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2026-06-17 17:03:04 -0300

    feat: create method isValid to verify uuid

--- src/domain/value-objects/uuid.php
+++ src/domain/value-objects/uuid.php
@@ -8,5 +8,11 @@
             
             return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
         }
+
+        public static function isValid(string $uuid): bool{
+            $pattern = '/^[0-9a-f]{8}-[0-9a-f]{4}-[1-7][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i';
+
+            return preg_match($pattern, $uuid) === 1;
+        }
     }
 ?>
